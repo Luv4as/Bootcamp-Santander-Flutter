ee7b04384ee593c769f2cd9b078cbd6c3c624f90
Lucas Venancio <lucas.venancio@outlook.com>
2023-09-18 16:53:32 -0300

    ultima aula de testes

--- meu_app_dart/lib/meu_app_dart.dart
+++ meu_app_dart/lib/meu_app_dart.dart
@@ -1,8 +1,29 @@
+import 'dart:convert';
 import 'dart:io';
-
 import 'package:meu_app_dart/exception/nome_invalido_exception.dart';
 import 'package:meu_app_dart/models/aluno.dart';
 import 'package:meu_app_dart/models/console_utils.dart';
+import 'package:http/http.dart' as http;
+
+double calcularDesconto(double valor, double desconto, bool percentual) {
+  if (valor <= 0) {
+    throw ArgumentError("O valor do produto não pode ser zero");
+  }
+  if (desconto <= 0) {
+    throw ArgumentError("O valor do desconto não pode ser zero");
+  }
+  if (percentual) {
+    return valor - ((valor * desconto) / 100);
+  }
+  return valor - desconto;
+}
+
+Future<Map<dynamic, dynamic>> retornarCEP(String cep) async {
+  var uri = Uri.parse("https://viacep.com.br/ws/$cep/json/");
+  var retorno = await http.get(uri);
+  var decodedResponse = jsonDecode(utf8.decode(retorno.bodyBytes)) as Map;
+  return decodedResponse;
+}
 
 void execute() {
   print("Bem vindo ao sistema de notas");
--- meu_app_dart/test/meu_app_dart_test.dart
+++ meu_app_dart/test/meu_app_dart_test.dart
@@ -1,7 +1,68 @@
+import 'package:meu_app_dart/meu_app_dart.dart' as app;
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(42, 42);
+  // esses individuais fazem a mesma coisa dos grupos
+
+  // test('Calcula o valor do produto com desconto sem porcentagem', () {
+  //   expect(app.calcularDesconto(1000, 150, false), 850);
+  // });
+
+  // test(
+  //     'Calcula o valor do produto com desconto sem porcentagem, passando o valor do produto zerado',
+  //     () {
+  //   expect(() => app.calcularDesconto(0, 150, false),
+  //       throwsA(TypeMatcher<ArgumentError>()));
+  // });
+
+  // test('Calcula o valor do produto com desconto com porcentagem', () {
+  //   expect(app.calcularDesconto(1000, 15, true), 850);
+  // });
+
+  // test('Calcula o valor do produto com desconto zerado', () {
+  //   expect(() => app.calcularDesconto(1000, 0, true),
+  //       throwsA(TypeMatcher<ArgumentError>()));
+  // });
+
+  group("Calcula o valor do produto com desconto: ", () {
+    var valuesToTest = {
+      {'valor': 1000, 'desconto': 150, 'percentual': false}: 850,
+      {'valor': 1000, 'desconto': 15, 'percentual': true}: 850,
+    };
+    valuesToTest.forEach((values, expected) {
+      test('$values: $expected', () {
+        expect(
+            app.calcularDesconto(
+                double.parse(values["valor"].toString()),
+                double.parse(values["desconto"].toString()),
+                values["percentual"] == true),
+            equals(expected));
+      });
+    });
+  });
+
+  group(
+      "Calcula o valor do produto informando valores zerados, deve gerar erro: ",
+      () {
+    var valuesToTest = {
+      {'valor': 0, 'desconto': 150, 'percentual': false},
+      {'valor': 1000, 'desconto': 0, 'percentual': true},
+    };
+    for (var values in valuesToTest) {
+      test('$values', () {
+        expect(
+            () => app.calcularDesconto(
+                double.parse(values["valor"].toString()),
+                double.parse(values["desconto"].toString()),
+                values["percentual"] == true),
+            throwsA(TypeMatcher<ArgumentError>()));
+      });
+    }
+  });
+
+  test('Retornar CEP', () async {
+    var body = await app.retornarCEP("01001000");
+    print(body);
+    expect(body["bairro"], equals("Sé"));
   });
 }
