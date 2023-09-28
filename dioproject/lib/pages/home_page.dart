61c770a3602717a944ab24868775980d574016a4
Lucas Venancio <lucas.venancio@outlook.com>
2023-09-27 21:56:47 -0300

    fix: tirando import desnecessário

--- dioproject/lib/pages/home_page.dart
+++ dioproject/lib/pages/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:dioproject/service/gera_num_aleatorio.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
